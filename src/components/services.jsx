c286156d9ce0365856e4d94adfc12d1b32c055ed
Santana Woollery <santanawoollery@gmail.com>
2021-06-04 15:15:51 -0400

    first commit

---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Our Services</h2>
+          <p>
+          <em>"Jessica takes away all the stress and pressure by crafting captions and graphics for social media posts that are engaging and relevant to my business." - Maria Inoa</em>
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
