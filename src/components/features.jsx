ec7b5ce906577306c1d4e09ac54a14d21896cf1a
Santana Woollery <santanawoollery@gmail.com>
2021-06-22 09:09:10 -0400

    Updated Features with button

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -6,23 +6,25 @@ export const Features = (props) => {
           <h2>Our Courses</h2>
         </div>
         <div className='row'>
-          {/*
-
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
                   {' '}
                   <i className={d.icon}></i>
+                  
                   <h3>{d.title}</h3>
-                  <p>{d.text}</p>
+                  <p>{d.text}
+                  </p>
                 </div>
               ))
-            : 'Loading...'}*/}
-            <div className='col-md-10 col-md-offset-1 section-title'>
-            <h1 style={{ color: '#608dfd' }}>COMING SOON!</h1>
-            </div>
+            : 'Loading...'}
         </div>
+        
       </div>
+      <a
+                  href='https://jmercermarketing.thinkific.com/courses/conquer-your-content'
+                  className='btn btn-custom btn-lg page-scroll'
+                >Enroll Now</a>
     </div>
   )
 }
